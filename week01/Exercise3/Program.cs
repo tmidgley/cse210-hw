5576c90ccfd6f8d6211451d6f0cbd8f957a893ea
tmidgley <travis.midgley@gmail.com>
2026-03-06 22:11:08 -0700

    completed exercise3 assignment with stretch goal

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Generate a random number between 1 and 100
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int attempts = 0;
+        
+        // For testing (steps 1 and 2)
+        //Console.WriteLine("What is the magic number? ");
+        //int magicNumber = Convert.ToInt32(Console.ReadLine());
+
+        // what is your guess?
+        Console.WriteLine("What is your guess? ");
+        int guess = Convert.ToInt32(Console.ReadLine());
+
+        //loop until the guess is correct
+        while (guess != number)
+        {
+            attempts++;
+             if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            Console.WriteLine("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"It took you {attempts} attempts.");
     }
 }
