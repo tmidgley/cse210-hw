acabbbff5b63140b8ed2405baeb165e9a5aa7cde
tmidgley <travis.midgley@gmail.com>
2026-03-06 21:53:27 -0700

    completed exercise2 with stretch goal

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //ask for grade percentage
+        Console.WriteLine("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int gradePercentageInt = int.Parse(gradePercentage);
+        // set variables for letter grade and sign
+        string letterGrade = "";
+        string sign = "";
+        // letter grade
+        if (gradePercentageInt >= 90)
+        {        
+            letterGrade = "A";
+        }    
+        else if (gradePercentageInt >= 80)
+        {        
+            letterGrade = "B";
+        }    
+        else if (gradePercentageInt >= 70)
+        {        
+            letterGrade = "C";
+        }    
+        else if (gradePercentageInt >= 60)
+        {        
+            letterGrade = "D";
+        }    
+        else
+        {        
+            letterGrade = "F";
+        }
+        // stretch goal +/-
+        int secondDigit = gradePercentageInt % 10;
+
+        if (letterGrade != "F")
+        {
+            if (secondDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (secondDigit <= 3)
+            {
+                sign = "-";
+            }
+        }
+        Console.WriteLine($"Your letter grade is {letterGrade}{sign}.");
+
+        // pass/fail
+        if (gradePercentageInt >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class.");
+        }
     }
 }
