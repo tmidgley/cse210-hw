f81f7611b6798378e8c691bfc77cb494db97e8e7
Travis Midgley <tmidgley@gmail.com>
2026-03-12 21:09:18 -0700

    W02 Learning Activity: Abstraction

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,17 +1,11 @@
+using System;
+
 public class Job
 {
     public string _company;
     public string _jobTitle;
     public int _startYear;
     public int _endYear;
-
-    public Job(string company, string jobTitle, int startYear, int endYear)
-    {
-        _company = company;
-        _jobTitle = jobTitle;
-        _startYear = startYear;
-        _endYear = endYear;
-    }
 
     public void Display()
     {
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "AWS";
+        job1._startYear = 2000;
+        job1._endYear = 2010;
+
+        //Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._startYear}-{job1._endYear}");
+
+        Job job2 = new Job();
+        job2._jobTitle = "AI Engineer";
+        job2._company = "Verint";
+        job2._startYear = 2010;
+        job2._endYear = 2020;
+
+        //Console.WriteLine($"{job2._jobTitle} ({job2._company}) {job2._startYear}-{job2._endYear}");
+
+        //job1.Display();
+        //job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Travis Midgley";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
