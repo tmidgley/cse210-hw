3b6d164344e2fcdd9cc735159949eac809dfa6bf
Travis Midgley <tmidgley@gmail.com>
2026-03-24 00:37:14 -0700

    youtube project with screenshot

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,21 @@
+public class Comment
+{
+    private string _name;
+    private string _commentText;
+
+    public Comment(string name, string commentText)
+    {
+        _name = name;
+        _commentText = commentText;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetCommentText()
+    {
+        return _commentText;
+    }
+}
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        Video video1 = new Video("5 Steps to Become a Billionaire", "Mark Cuban", 123);
+        Video video2 = new Video("Mr. Beast Goes to Mars", "Mr. Beast", 456);
+        Video video3 = new Video("Best Dunks of the 2000's", "Michael Jordan", 789);
+
+        List<Video> videos = new List<Video>();
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        // Add comments to video1
+        video1.AddComment(new Comment("Alice", "Great video!"));
+        video1.AddComment(new Comment("Bob", "Super helpful!"));
+        video1.AddComment(new Comment("Charlie", "I learned a lot!"));
+
+        // Add comments to video2
+        video2.AddComment(new Comment("Dave", "This was amazing!"));
+        video2.AddComment(new Comment("Eve", "Loved it!"));
+        video2.AddComment(new Comment("Frank", "Sign me up next!"));
+
+        // Add comments to video3
+        video3.AddComment(new Comment("Gabe", "GOAT!"));
+        video3.AddComment(new Comment("Hannah", "SKYWALKER!"));
+        video3.AddComment(new Comment("Ian", "Make more videos!"));
+
+        foreach (Video v in videos)
+        {
+            Console.WriteLine("----- VIDEO -----");
+            Console.WriteLine($"Title: {v.GetTitle()}");
+            Console.WriteLine($"Author: {v.GetAuthor()}");
+            Console.WriteLine($"Length: {v.GetLength()} seconds");
+            Console.WriteLine($"Comments: {v.GetCommentCount()}");
+
+            foreach (Comment c in v.GetComments())
+            {
+                Console.WriteLine($"{c.GetName()}: {c.GetCommentText()}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
